415f2bceec5d8bee883eb6bd4a58a7b16f485d4a
javaclll <yugu.upadhyaya@gmail.com>
2024-03-19 14:18:27 +0545

    chore: poetry init and torch usage

---
+++ example.py
@@ -0,0 +1,38 @@
+import torch
+import numpy
+import math
+
+dtype = torch.float
+device = torch.device("cpu")
+# device = torch.device("cuda:0") # Uncomment this to run on GPU
+
+x = numpy.linspace(-math.pi, math.pi, 2000)
+y = numpy.sin(x)
+
+a = torch.randn((), device=device, dtype=dtype)
+b = torch.randn((), device=device, dtype=dtype)
+c = torch.randn((), device=device, dtype=dtype)
+d = torch.randn((), device=device, dtype=dtype)
+
+learningRate = 1e-6
+
+for i in range(2000):
+    yPred = a + b * x + c * x + x ** 2 + d + x ** 3 
+    loss = numpy.square(yPred - y).pow(2).sum().item()
+
+    if i % 100 == 99:
+        print(i, loss)
+
+    gradYPred = 2.0 * (yPred - y)
+    grada = gradYPred.sum()
+    gradb = (gradYPred * x).sum()
+    gradc = (gradYPred * x ** 2).sum()
+    gradd = (gradYPred * x ** 3).sum()
+
+    a -= learningRate * grada
+    b -= learningRate * gradb
+    c -= learningRate * gradc
+    d -= learningRate * gradd
+
+    print(f"Result: y =  {a.item()} + {b.item()} x + {c.item()} x^2 + {d.item()} x^3")
+
