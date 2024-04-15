2dd2a0867d879f459df1129c13bb5fdbb3395c3a
javaclll <yugu.upadhyaya@gmail.com>
2024-04-15 22:25:22 +0545

    feat: meter and scale mapping added

--- dataset_mask.py
+++ dataset_mask.py
@@ -1,7 +1,7 @@
 import cv2
 from segmentation.kmeans import findKMeans
 
-for i in range(1, 83):
+for i in range(1, 101):
   image = cv2.imread(f"./images/image_{i}.png")
   masked_image = findKMeans(image)
   cv2.imwrite(f"./masks/image_{i}.png", masked_image)
